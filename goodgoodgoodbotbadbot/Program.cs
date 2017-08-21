e3796638db6c95cfdacd35958cf75f4603c60411
JuhaJGamer <juhajgamer@live.fi>
2017-08-21 16:42:17 +0300

    fixed it lol

--- goodgoodgoodbotbadbot/Program.cs
+++ goodgoodgoodbotbadbot/Program.cs
@@ -28,41 +28,36 @@ namespace goodgoodgoodbotbadbot
             }
 
             //Reddit code
-            var webAgent = new BotWebAgent("Good_Good_GB_BB", "newpass", "tLNL8oa6HZkPZA", "99QLr7S3F2CjzJDGA6zvpTVwx5M", "no://no.no");
+            var webAgent = new BotWebAgent("Good_Good_GB_BB", "newpass", "1zqbIrqwygfySg", "2oHOvib5wBKMmaHEpW9C4QU3Q7M", "localhost:8080");
             //This will check if the access token is about to expire before each request and automatically request a new one for you
             //"false" means that it will NOT load the logged in user profile so reddit.User will be null
-            var reddit = new Reddit(webAgent, false);
-            var subreddit = reddit.GetSubreddit("/r/all");
-            subreddit.Subscribe();
+            var reddit = new Reddit(webAgent, true);
+            var subreddit = reddit.RSlashAll;
             while (true)
             {
-                foreach (var post in subreddit.New.Take(25))
+                foreach (var c in subreddit.Comments)
                 {
-                    if (post.Title == "What is my karma?")
+                    try
                     {
-                        // Note: This is an example. Bots are not permitted to cast votes automatically.
-                        post.Upvote();
-                        var comment = post.Comment(string.Format("You have {0} link karma!", post.Author.LinkKarma));
-                        comment.Distinguish(VotableThing.DistinguishType.Moderator);
-                    }
-                    foreach(var comm in post.Comments)
-                    {
-                        Console.Write("Comment author:{0}", comm.AuthorName);
-                        if(comm.AuthorName == "Good_GoodBot_BadBot")
+                        Console.Write("Comment author:{0}", c.AuthorName);
+                        Console.Write(" || Parent author:{0}\n", ((Comment)reddit.GetThingByFullname(c.ParentId)).AuthorName);
+                        if (((Comment)reddit.GetThingByFullname(c.ParentId)).AuthorName == "Good_GoodBot_BadBot")
                         {
-                            foreach(Comment c in comm.Comments)
+                            if (c.Body.ToLower() == "good bot" || c.Body.ToLower() == "good bot.")
                             {
-                                if (c.Body.ToLower() == "good bot" || c.Body.ToLower() == "good bot.")
-                                {
-                                    times++;
-                                    var comment = c.Reply(string.Format("You are the {0}^th person calling /u/Good_GoodBot_BadBot a good bot. HE definetly is a little less awesome!", times));
-                                    writef(times);                              
-                                }
+                                times++;
+                                var comment = c.Reply(string.Format("You are the {0}^th person calling /u/Good_GoodBot_BadBot a good bot. HE definetly is a little less awesome!", times));
+                                Console.WriteLine("Commented: {0}", times);
+                                writef(times);
                             }
                         }
                     }
+                    catch (InvalidCastException)
+                    {
+                        Console.WriteLine(" || Not a comment");
+                    }
                 }
-                Thread.Sleep(10);
+                Thread.Sleep(500);
             }
         }
 
